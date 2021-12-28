07a7f003524ba42c99ec20d020cd460bed1e2d2a
Maxim Socol <maxim.s@alsodev.md>
2021-12-28 04:13:40 +0200

    add sliders slick

--- src/js/app.js
+++ src/js/app.js
@@ -7,13 +7,35 @@ import 'slick-carousel'
     
 window.jQuery = window.$ = $; 
 
-$('.products').slick({
+$('.hits__products').slick({
     dots: false,
     infinite: false,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 4,
     arrows: true,
-    prevArrow: $('.arr__prev'),
-    nextArrow: $('.arr__next'),
+    prevArrow: $('.arr__prev_hits'),
+    nextArrow: $('.arr__next_hits'),
+});
+
+$('.goodies__products').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: true,
+    prevArrow: $('.arr__prev_goodies'),
+    nextArrow: $('.arr__next_goodies'),
+});
+
+$('.toys__products').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: true,
+    prevArrow: $('.arr__prev_toys'),
+    nextArrow: $('.arr__next_toys'),
 });
