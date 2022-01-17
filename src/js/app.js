3470e4244c01c5e97d238b54c2cae62a28d77dc1
Maxim Socol <maxim.s@alsodev.md>
2022-01-18 00:53:49 +0200

    Add components

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
 import * as flsFunctions from "./modules/functions.js";
 import * as slickSlider from "./modules/slick.js";
+import * as scripts from "./modules/scripts.js";
 
 flsFunctions.isWebp();
 slickSlider.slickJs();
+scripts.scripts();
+
---
+++ src/js/modules/scripts.js
@@ -0,0 +1,15 @@
+const popupRegister = document.getElementById('register-popup'),
+    iconAuthRegister = document.getElementById('auth-register'),
+    closePopupRegister = document.getElementById('close-register-popup');
+
+    // if(navToggle) {
+    //     navToggle.addEventListener('click', () => {
+    //         navMenu.classList.add('show-menu')
+    //     })
+    // }
+
+    // if(navClose) {
+    //     navClose.addEventListener('click', () => {
+    //         navMenu.classList.remove('show-menu')
+    //     })
+    // }
